1b0b98c75ef4c6701bd061f85867470260b2a363
Zeus, The True God <l-2732@yandex.ru>
2011-10-09 17:16:23 +0400

    drawing hexagon tiles

--- resources/js/App.js
+++ resources/js/App.js
@@ -2,4 +2,9 @@ $(function(){
 	var mapPlaceholder = $('<div></div>');
 	$('body').append(mapPlaceholder);
 	var map = new Civ.Map(mapPlaceholder);
+	map.setItems({
+		size: 72,
+		rows: 10,
+		cols: 20
+	});
 });
--- resources/js/Map.js
+++ resources/js/Map.js
@@ -1,9 +1,22 @@
 //The map class should just generate a set of hex items
 window.Civ = window.Civ || {};
-Civ.Map = function(el){
-	this.el = el;
-	this.createMarkup();
+Civ.Map = function(el) {
+  this.el = el;
+  this.createMarkup();
 };
-Civ.Map.prototype.createMarkup = function(){
-	this.el.addClass('map-container');
-}
+Civ.Map.prototype.createMarkup = function() {
+  this.el.addClass('map-container');
+};
+Civ.Map.prototype.setItems = function(config) {
+  var x, y;
+  for (x = 0; x < config.cols; x++) {
+    for (y = 0; y < config.rows; y++) {
+      var img = $('<img></img>');
+      img.addClass('hex');
+			img.css('left', x * config.size * 0.75 + 'px').css('top', y * config.size + (x % 2) * 0.5 * config.size + 'px');
+			img.css('width', config.size + 'px').css('height', config.size + 'px');
+			img.attr('src','http://perludus.com/examples/hexmap/hills-variation3.png');
+			this.el.append(img);
+    }
+  }
+};
