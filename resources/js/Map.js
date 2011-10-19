564c7238edb42395ad7530e3a2f47367964c4cc8
Zeus, The True God <l-2732@yandex.ru>
2011-10-19 18:45:03 +0400

    making partial map updates

--- resources/js/Map.js
+++ resources/js/Map.js
@@ -39,38 +39,66 @@ $.extend(Civ.Map.prototype, {
         window.console && window.console.info(config);
         var me = this;
         var s = config.size;
+        var canUseExistingMarkup = this.itemsConfig && this.itemsConfig.size === config.size && this.itemsConfig.cols === config.cols && this.itemsConfig.rows === config.rows;
+        this.previousItemsConfig = this.itemsConfig;
         this.itemsConfig = config;
-        this.el.html('');
-        var container = $('<div></div>').css('position', 'relative').css('overflow', 'hidden').addClass('no-select');
-        container.width(config.size * config.cols * 0.75 + config.size * 0.25);
-        container.height(config.size * config.rows + config.size * 0.5);
-        container.svg(function(svg) {
-            var defs = svg.defs();
-            var clipMask = svg.clipPath('hex-clip', null);
-            $(defs).append(clipMask);
-            $(clipMask).append(svg.polygon([
-                [0, s / 2],
-                [s / 4, 0],
-                [3 * s / 4, 0],
-                [s, s / 2],
-                [3 * s / 4, s],
-                [s / 4, s]
-            ], {
-                strokeWidth: 5
-            }));
-            var x, y;
-            for (x = 0; x < config.cols; x++) {
-                for (y = 0; y < config.rows; y++) {
-                    var tile = config.map[y][x];
-                    var tileEl = me.createTile(svg, {
-                        x: x,
-                        y: y,
-                        size: config.size,
-                        cellInfo: tile
-                    });
-                }
-            }
-        });
+        if (!canUseExistingMarkup) {
+            this.tileCache = {};
+            this.el.html('');
+            var container = $('<div></div>').css('position', 'relative').css('overflow', 'hidden').addClass('no-select');
+            this.container = container;
+            container.width(config.size * config.cols * 0.75 + config.size * 0.25);
+            container.height(config.size * config.rows + config.size * 0.5);
+            container.svg(function(svg) {
+                me.svg = svg;
+                var defs = svg.defs();
+                var clipMask = svg.clipPath('hex-clip', null);
+                $(defs).append(clipMask);
+                $(clipMask).append(svg.polygon([
+                    [0, s / 2],
+                    [s / 4, 0],
+                    [3 * s / 4, 0],
+                    [s, s / 2],
+                    [3 * s / 4, s],
+                    [s / 4, s]
+                ], {
+                    strokeWidth: 5
+                }));
+                me.updateTiles();
+            });
+        } else {
+            me.updateTiles();
+        }
         this.el.append(container);
+    },
+    updateTiles: function() {
+        var config = this.itemsConfig;
+        var x, y;
+        for (x = 0; x < config.cols; x++) {
+            for (y = 0; y < config.rows; y++) {
+                var tile = config.map[y][x];
+                this.updateTile(this.svg,{
+                    x: x,
+                    y: y,
+                    size: config.size,
+                    cellInfo: tile
+                });
+            }
+        }
+    },
+    updateTile: function(svg, cell) {
+        var key = cell.x + ':' + cell.y;
+        var previousValue = this.tileCache[key];
+        if (previousValue) {
+            if (JSON.stringify(cell) === this.tileCache[key].cell) {
+                return;
+            }
+            $(previousValue.el).remove();
+        }
+        var el = this.createTile(svg, cell);
+        this.tileCache[key] = {
+            cell: JSON.stringify(cell),
+            el: el
+        };
     }
 });
